330ed8c2843c08ddf9dee14ab4482aaa71b7b90c
tmin002 <ssh9930@gmail.com>
2022-01-15 17:58:13 +0900

    Add map data/settings r/w feature

---
+++ Miner/Assets/Scenes/chooseMapAsset/chooseMapInit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Android;
+
+public class chooseMapInit : MonoBehaviour
+{
+
+    void Start()
+    {
+        Map.loadLocalMaps();
+        Map.deleteLocalMap(Map.localMaps[0]);
+    }
+
+    // called when the game is terminated
+    void OnDisable()
+    {
+        Debug.Log("OnDisable");
+    }
+
+    // Debug.Log("경로: " + Application.persistentDataPath);
+
+}
