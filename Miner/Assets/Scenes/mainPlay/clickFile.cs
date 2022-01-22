7a6a917c7276cdbe48c73c50d08727ce9ac8fc2b
dlgusrb3456 <dlgusrb3456@naver.com>
2022-01-22 16:41:00 +0900

    현규 - (메인_플레이 스크립트 작성 시작, 프리팹 이용해서 페이지당 4개씩 추가, 파일 클릭시 정보 판넬 나옴(ui수정 필), 정보 판넬에 정보 할당은 아직 못함)

---
+++ Miner/Assets/Scenes/mainPlay/clickFile.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+class userInfo
+{
+    public string nickName { get; set; }
+    public int playTime { get; set; }
+
+    public userInfo(string nickName, int playTime)
+    {
+        this.nickName = nickName;
+        this.playTime = playTime;
+    }
+
+    public override string ToString()
+    {
+        return $"--nickName : {nickName}, \t playTime={playTime}--";
+    }
+}
+
+
+public class clickFile : MonoBehaviour
+{
+
+    public GameObject Panel_fileInfo;
+    public GameObject Button_PlayFile;
+    private GameObject fileInfo;
+    public Text Text_puPri;
+
+
+    
+
+
+
+    public void fileOnClick()
+    {
+        //api 호출을 통해 다음의 정보를 가져와야함.
+        //1. 미로명 2. private, public 상태, 3. 크기정보 , 4. 평균 플레이 타임, 5. 플레이한 사람들의 정보 (닉네임과 플레이타임) => 빠른 순서대로 정렬해야함.
+        string miroName = "미로 1";
+        string isPrivate = "private";
+        string size = "작음";
+        string playTime = "1m 35s";
+        List<userInfo> users = new List<userInfo>();
+        users.Add(new userInfo ("라큼",25));
+        users.Add(new userInfo("라캄", 15));
+        users.Add(new userInfo("라콤", 5));
+        users.Add(new userInfo("라쿰", 85));
+
+        users = users.OrderBy(x => x.playTime).ToList();
+        foreach(var userInfo in users)
+        {
+            Debug.Log(userInfo.playTime);
+        }
+
+        fileInfo = Instantiate(Panel_fileInfo) as GameObject;
+        fileInfo.transform.parent = Button_PlayFile.transform.parent;
+        fileInfo.transform.position = new Vector3(0,0,0);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Miner/Assets/Scenes/mainPlay/mainPlay.cs
+++ Miner/Assets/Scenes/mainPlay/mainPlay.cs
@@ -1,11 +1,127 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class mainPlay : MonoBehaviour
 {
+    //미로파일 4개 Panel
+    public GameObject Button_PlayFile;
+    public GameObject Panel_files;
+    public Sprite image_lock;
+    public Sprite image_unLock;
 
-    
+    //search 변수
+    public InputField searchInputField;
+
+
+    //Dropdown 변수
+    public Dropdown dropdown;
+    private int chooseSearchType = 0; //0이면 닉네임 / 1이면 미로명
+
+    //Toggle
+    public Toggle Toggle_popular;
+    public Toggle Toggle_latest;
+
+    public void LoadfilestoPanel()
+    {
+        //기존의 판넬에 들어있는 파일들 모두 삭제.
+        Transform[] childList = Panel_files.GetComponentsInChildren<Transform>();
+        if (childList != null)
+        {
+            for (int i = 1; i < childList.Length; i++)
+            {
+                if (childList[i] != transform)
+                {
+                    Destroy(childList[i].gameObject);
+                }
+            }
+        }
+        // 선택한 페이지에 속한 파일 정보를 가져와야함.
+
+        for(int i = 0; i < 4; i++) //가져온 파일의 개수가 4개인 경우, List형식으로 반환할 예정
+        {
+            GameObject file = Instantiate(Button_PlayFile) as GameObject;
+            file.transform.parent = Panel_files.transform;
+            file.transform.GetChild(0).GetComponent<Text>().text = "미로 " + i + "/ 락큼";
+            if(i%2 == 0) // 이 미로가 공유파일인지 확인하는 절차 => 공유인경우
+            {
+                file.transform.GetChild(1).GetComponent<Image>().sprite = image_unLock;
+            }
+            else
+            {
+                file.transform.GetChild(1).GetComponent<Image>().sprite = image_lock;
+            }
+            file.transform.GetChild(2).GetComponent<Text>().text = "총 플레이 :  " + i;
+        }
+
+    }
+
+    public void getFileInfo(int pageNum)
+    {
+        int fileCount = 0;
+    }
+
+
+    public void searchNull()
+    {
+        searchInputField.text = "";
+        if (Toggle_popular.isOn)
+        {
+            //인기순으로 검색해서 화면에 보이기
+        }
+        else
+        {
+            //최신순으로 검색해서 화면에 보이기
+        }
+    }
+
+    public void searchNotNull(string text)
+    {
+        int isPopular = -1;
+        int isNickName = chooseSearchType;
+
+        if (Toggle_popular.isOn)
+        {
+            isPopular = 0; // 인기순
+        }
+        else
+        {
+            isPopular = 1; // 최신순
+        }
+        // isPopular, isNickName, text 세가지 넘겨서 값 받아오기
+
+
+    }
+
+
+    public void search()
+    {
+        for(int i = 0; i < searchInputField.text.Length; i++)
+        {
+            if(searchInputField.text[i]==' ')
+            {
+                Debug.Log("띄어쓰기는 입력할 수 없습니다.");
+                return;
+            }
+        }
+
+        if(searchInputField.text.Length == 0)
+        {
+            searchNull();
+        }
+        else
+        {
+            searchNotNull(searchInputField.text);
+        }
+
+    }
+
+    public void searchDelete()
+    {
+        searchNull();
+    }
+
+
 
     public void getPagingInfo()
     {
@@ -21,11 +137,17 @@ public class mainPlay : MonoBehaviour
 
 
     }
+    public void OnDropdownEvent(int index)
+    {
+        chooseSearchType = index;
+        Debug.Log(chooseSearchType);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        dropdown.onValueChanged.AddListener(OnDropdownEvent);
+        LoadfilestoPanel();
     }
 
     // Update is called once per frame
