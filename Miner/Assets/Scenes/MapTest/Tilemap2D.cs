08da7ae59f6d42f1c3b5c375a0ef616a35a40964
tmf2377 <tmf2377@naver.com>
2022-01-24 02:02:23 +0900

    jsontest seoa

---
+++ Miner/Assets/Scenes/MapTest/Tilemap2D.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class Tilemap2D : MonoBehaviour
+{
+    /*[Header("Tile")]
+    [SerializeField]
+    private GameObject tilePrefeb;
+
+    public void GenerateTilemap(MapData mapData)
+    {
+        int width = mapData.mapSize.x;
+        int width = mapData.mapSize.y;
+
+        for (int y = 0; y < height; ++y)
+        {
+            for ( int x = 0; x < width; ++x)
+            {
+                int index = y * width + x;
+
+                if (mapData.mapData[index] == (int)TileType.Empty)
+                {
+                    continue;
+                }
+
+                Vector3 position = new Vector3(-(width * 0.5f - 0.5f) + x, (height * 0.5f - 0.5f) - y);
+
+                if (mapData.mapData[index] > (int)TileType.Empty && mapData.mapData[index] < (int)TileType.LastIndex)
+                {
+                    SpawnTile((TileType) mapData.mapData[index], position);
+                }
+            }
+        }
+
+    }
+
+    private void SpawnTile(TileType tileType, Vector3 position)
+    {
+        GameObject clone = Instantiate(tilePrefab, posotion, Quaternion.identity);
+
+        clone.name = "Tile";
+        clone.transform.SetParent(transform);
+
+        Tile tile = clone.GetComponent<Tile>();
+        tile.Setup(tileType);
+    }*/
+}
