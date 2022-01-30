0887c3f97dd726dbd22b0f979a75b960b071a2fe
tmf2377 <tmf2377@naver.com>
2022-01-31 03:18:28 +0900

    돌아가기 버튼,종료 seoa

---
+++ Miner/Assets/Scenes/InGamePlay/PlayerAction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    public float Speed;
+
+    Rigidbody2D rigid;
+    float h;
+    float v;
+    bool isHorizonMove;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+     // rigid.velocity = new moveVec * Speed;
+    }
+}
