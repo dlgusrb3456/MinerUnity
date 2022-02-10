43845f518298ab96e73c029d07fc2d4afb4fd728
dlgusrb3456 <dlgusrb3456@naver.com>
2022-02-10 22:32:32 +0900

    현규- 맵 뒤집혀보이는거 수정

--- Miner/Assets/Scenes/InGamePlay/mapMaker.cs
+++ Miner/Assets/Scenes/InGamePlay/mapMaker.cs
@@ -133,7 +133,7 @@ public class mapMaker : MonoBehaviour
             for (int j = 0; j < designArr.GetLength(1); j++)
             {
 
-                Vector3 position = new Vector3(-1.0f + (float)(-0.6) * i, -1.0f + (float)0.6 * j, 0);
+                Vector3 position = new Vector3(-1.0f + (float)(0.6) * i, -1.0f + (float)(-0.6) * j, 0);
                 if (designArr[i, j] == 1)
                 {
                     grass_obj = Instantiate(grassPrefab);
@@ -212,7 +212,7 @@ public class mapMaker : MonoBehaviour
             for (int j = 0; j < designArr.GetLength(1); j++)
             {
              
-                Vector3 position = new Vector3(-1.0f + (float)(-0.6) * i, -1.0f + (float)0.6 * j, 0);
+                Vector3 position = new Vector3(-1.0f + (float)(0.6) * j, -1.0f + (float)(-0.6) * i, 0);
                 if (designArr[i, j] == 1)
                 {
                     grass_obj = Instantiate(grassPrefab);
